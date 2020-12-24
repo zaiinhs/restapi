36656e94721a441aa38c5894a19f4e98ce096a5b
zain <zainalabidinhsc@gmail.com>
2020-12-24 09:49:49 +0700

    berhasil menampilkan data dari database

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function (req, res) {
-    response.ok("Aplikasi berjalan")
+    response.ok("Aplikasi berjalan", res)
+};
+
+//menampilkan data
+exports.tampildata = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
 };
