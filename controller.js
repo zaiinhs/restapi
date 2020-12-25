bdc23084eadcc45ad1454dce2ba5e9869ccc88af
zain <zainalabidinhsc@gmail.com>
2020-12-25 11:08:11 +0700

    tambah data

--- controller.js
+++ controller.js
@@ -30,3 +30,20 @@ exports.tampildataberdasarkanid = function (req, res) {
             }
         });
 };
+
+//menambahkan data mahasiswa
+exports.tambahdata = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?,?,?)',
+        [nim, nama, jurusan],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("berhasil di tambahkan", res)
+            }
+        });
+};
--- routes.js
+++ routes.js
@@ -11,4 +11,6 @@ module.exports = function (app) {
 
     app.route('/tampil/:id')
         .get(jsonku.tampildataberdasarkanid);
+    app.route('/tambah')
+        .post(jsonku.tambahdata);
 }
