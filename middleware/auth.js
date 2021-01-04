5356f517c9eda0ce1dc46beb3883c9b4b2f9105b
zaiinhs <zainalabidin.tongas@gmail.com>
2021-01-04 20:47:30 +0700

    update belajar web token jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
-var connection = require('../koneksi');
-var mysql = require('mysql');
-var md5 = require('MD5');
-var response = require('../res');
-var jwt = require('jsonwebtoken');
-var config = require('../config/secret');
-var ip = require('ip');
+var connection = require("../koneksi");
+var mysql = require("mysql");
+var md5 = require("MD5");
+var response = require("../res");
+var jwt = require("jsonwebtoken");
+var config = require("../config/secret");
+var ip = require("ip");
 
 //controller untuk register
 exports.registrasi = function (req, res) {
@@ -16,10 +16,10 @@ exports.registrasi = function (req, res) {
         tanggal_daftar: new Date()
     }
 
-    var query = "SELECT email FROM ?? WHERE ??";
+    var query = "SELECT email FROM ?? WHERE ??=?";
     var table = ["user", "email", post.email];
 
-    query = mysql.format(query.table);
+    query = mysql.format(query, table);
 
     connection.query(query, function (error, rows) {
         if (error) {
@@ -37,8 +37,62 @@ exports.registrasi = function (req, res) {
                     }
                 });
             } else {
-                response.ok("Email sudah terdaftar!");
+                response.ok("Email sudah terdaftar!", res);
             }
         }
     })
+}
+
+// controller untuk login
+exports.login = function (req, res) {
+    var post = {
+        password: req.body.password,
+        email: req.body.email
+    }
+
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["user", "password", md5(post.password), "email", post.email];
+
+    query = mysql.format(query, table);
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 1) {
+                var token = jwt.sign({ rows }, config.secret, {
+                    expiresIn: 1440
+                });
+                id_user = rows[0].id;
+
+                var data = {
+                    id_user: id_user,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["akses_token"];
+
+                query = mysql.format(query, table);
+                connection.query(query, data, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        res.json({
+                            success: true,
+                            message: 'Token JWT berhasil!',
+                            token: token,
+                            currUser: data.id_user
+                        });
+                    }
+                });
+            } else {
+                res.json({ "error": true, "Message": "Email atau password salah!" });
+            }
+        }
+    });
+}
+
+exports.halamanrahasia = function (req, res) {
+    response.ok("Halaman ini untuk user role = 2 !", res);
 }
---
+++ middleware/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var auth = require('./auth');
+var router = express.Router();
+var verifikasi = require('./verifikasi');
+
+//daftarkan menu registrasi
+router.post('/api/v1/register', auth.registrasi);
+router.post('/api/v1/login', auth.login);
+
+// Alamat yang perlu otorisasi
+router.get('/api/v1/rahasia', verifikasi(), auth.halamanrahasia);
+
+module.exports = router;
